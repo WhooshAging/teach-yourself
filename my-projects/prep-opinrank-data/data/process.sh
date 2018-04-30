ae422067d4bfe9c9b2914631bd45eaa1b95865e9
WhooshAging <bangkockney@gmail.com>
2018-04-30 01:52:48 +0100

    samsung ssh data processed

--- my-projects/prep-opinrank-data/data/process.sh
+++ my-projects/prep-opinrank-data/data/process.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 cd 'in'
+n=1;
 for i in * 
 do
-	grep '^<TEXT>' "$i" | sed 's/<TEXT>//g' | sed 's/<\/TEXT>//g' > "$i"'.txt';
+	grep '^<TEXT>' "$i" | sed 's/<TEXT>//g' | sed 's/<\/TEXT>//g' > "$n"'.txt';
+	n=$((n+1));
 done
 mv *.txt ../out
